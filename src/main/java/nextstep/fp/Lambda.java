ca85ec6130d625bef8918107ce4555af3dfdcb4e
gyeongman <rudaks012@naver.com>
2022-09-13 21:45:52 +0900

    Refactor : sumAllEven method lamda 변경

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -1,6 +1,7 @@
 package nextstep.fp;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lambda {
     public static void printAllOld(List<Integer> numbers) {
@@ -27,21 +28,14 @@ public class Lambda {
     }
 
     public static int sumAll(List<Integer> numbers) {
-//        int total = 0;
-//        for (int number : numbers) {
-//            total += number;
-//        }
         return numbers.stream().reduce(0, Integer::sum);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return numbers.stream()
+                .filter(number -> number % 2 == 0)
+                .mapToInt(Integer::intValue)
+                .sum();
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
