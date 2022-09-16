2c0030e1219cbc6743c92c7c12d17e6d9c0bbe50
gyeongman <rudaks012@naver.com>
2022-09-16 20:16:08 +0900

    Test : 게임참석자 2명 미만 테스트 코드 작성

--- src/main/java/step2/domain/GameParticipants.java
+++ src/main/java/step2/domain/GameParticipants.java
@@ -9,8 +9,8 @@ public class GameParticipants {
     private final List<Ladder> ladders;
 
     public GameParticipants(final String[] names) {
-        if (names.length > 2) {
-            throw new IllegalArgumentException();
+        if (names.length < 2) {
+            throw new IllegalArgumentException("게임 참석자가 2명 미만이면 예외가 발생한다.");
         }
         this.ladders = createLadder(names);
     }
--- src/test/java/step2/GameParticipantsTest.java
+++ src/test/java/step2/GameParticipantsTest.java
@@ -1,4 +1,21 @@
 package step2;
 
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.ThrowableAssert;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import step2.domain.GameParticipants;
+
 public class GameParticipantsTest {
+
+    @Test
+    @DisplayName("게임 참석자가 2명 미만이면 예외가 발생한다.")
+    void throw_exception_game_participants_two_more() {
+        // given
+        final ThrowableAssert.ThrowingCallable actual = () -> new GameParticipants(new String[]{"1"});
+
+        Assertions.assertThatThrownBy(actual)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("게임 참석자가 2명 미만이면 예외가 발생한다.");
+    }
 }
