2de262111735803e0016c269b81100bed0549f13
gyeongman <rudaks012@naver.com>
2022-09-16 23:11:41 +0900

    Test : Ladder 넓이, 높이 1이상 추가

--- src/main/java/step2/domain/Ladder.java
+++ src/main/java/step2/domain/Ladder.java
@@ -8,10 +8,11 @@ public class Ladder {
     private List<Line> lines = new ArrayList<>();
 
     public Ladder(int width, int height) {
+        if (width < 1 && height < 1) {
+            throw new IllegalArgumentException();
+        }
         for (int i = 0; i < height; i++) {
             lines.add(Line.of(width));
         }
-
     }
-
 }
---
+++ src/test/java/step2/domain/LadderTest.java
@@ -0,0 +1,18 @@
+package step2.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LadderTest {
+
+    @Test
+    @DisplayName("높이와 넓이는 1 이상이여야 한다.")
+    void width_and_height_is_must_least_one() {
+        int width = 0;
+        int height = 0;
+        Assertions.assertThatThrownBy(() -> new Ladder(width,height))
+                .isInstanceOf(IllegalArgumentException.class);
+
+    }
+}
