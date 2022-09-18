22161609849935967808de32c37f9fa4b7ef0a78
gyeongman <rudaks012@naver.com>
2022-09-18 16:38:39 +0900

    Test : 참석자 2명이상 테스트 코드 작성

--- src/test/java/step2/domain/GameParticipantsTest.java
+++ src/test/java/step2/domain/GameParticipantsTest.java
@@ -1,9 +1,11 @@
 package step2.domain;
 
-import org.assertj.core.api.Assertions;
 import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class GameParticipantsTest {
 
@@ -13,8 +15,20 @@ public class GameParticipantsTest {
         // given
         final ThrowableAssert.ThrowingCallable actual = () -> new GameParticipants(new String[]{"1"});
 
-        Assertions.assertThatThrownBy(actual)
+        assertThatThrownBy(actual)
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("게임 참석자가 2명 미만이면 예외가 발생한다.");
+    }
+
+    @Test
+    @DisplayName("게임 참석자가 2명 이상이어야 한다.")
+    void game_participant_two_more() {
+        // given
+        GameParticipants gameParticipants = new GameParticipants(new String[]{"123", "456", "789"});
+        // when
+        int actual = gameParticipants.getParticipants().size();
+        int expected = 3;
+        // then
+        assertThat(actual).isEqualTo(expected);
     }
 }
